382d32484b97c5f21a56f070abdfabaa2ed51aa7
Tristin Barnett <tristinbarnett@gmail.com>
2020-04-15 12:28:38 -0700

    added schema and seeds for db

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('BBQ Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Spicy Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
